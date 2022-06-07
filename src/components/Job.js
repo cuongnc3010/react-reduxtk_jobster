b14fda5cf4c2e004de97f5af28c032f0c2cfb4b6
cuongnc <codondemen1102@gmail.com>
2022-06-07 08:42:57 +0700

    get allJobs functionality added

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,4 +1,64 @@
-const Job = () => {
-  return <h4>Job</h4>
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/Job'
+import { useDispatch } from 'react-redux'
+import JobInfo from './JobInfo'
+import moment from 'moment'
+import { deleteJob } from '../features/job/jobSlice'
+
+const Job = (job) => {
+  const {
+    _id: id,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    createdAt,
+    status,
+  } = job
+  const dispatch = useDispatch()
+  const date = moment(createdAt).format('MMM Do, YYYY')
+  return (
+    <Wrapper>
+      <header>
+        <div className='main-icon'>{company.charAt(0)}</div>
+        <div className='info'>
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className='content'>
+        <div className='content-center'>
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+
+          <div className={`status ${status}`}> {status}</div>
+        </div>
+        <footer>
+          <div className='action'>
+            <Link
+              to='/add-job'
+              className='btn edit-btn'
+              onClick={() => {
+                console.log('edit job')
+              }}
+            >
+              edit
+            </Link>
+            <button
+              type='button'
+              className='btn delete-btn'
+              onClick={() => {
+                dispatch(deleteJob(id))
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  )
 }
 export default Job
--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,4 +1,41 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Job from './Job'
+import Wrapper from '../assets/wrappers/JobsContainer'
+import Loading from './Loading'
+import { getAllJobs } from '../features/allJobs/allJobsSlice'
+
 const JobsContainer = () => {
-  return <h3>JobsContainer</h3>
+  const { jobs, isLoading } = useSelector((store) => store.allJobs)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAllJobs())
+  }, [])
+
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Loading center />
+      </Wrapper>
+    )
+  }
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No job to display...</h2>
+      </Wrapper>
+    )
+  }
+  return (
+    <Wrapper>
+      <h5>job info</h5>
+      <div className='jobs'>
+        {jobs.map((job) => (
+          <Job key={job._id} {...job} />
+        ))}
+      </div>
+    </Wrapper>
+  )
 }
 export default JobsContainer
