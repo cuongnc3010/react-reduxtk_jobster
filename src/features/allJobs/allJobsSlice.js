d9ca9d6e6efc7b822489ac5e486ba8fae413d2e4
cuongnc <codondemen1102@gmail.com>
2022-06-08 09:00:21 +0700

    get Stats request addad

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -2,13 +2,13 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { toast } from 'react-toastify'
 import customFetch from '../../utils/axios'
 
-const initialFiltersState = {
-  search: '',
-  searchStatus: 'all',
-  searchType: 'all',
-  sort: 'latest',
-  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
-}
+// const initialFiltersState = {
+//   search: '',
+//   searchStatus: 'all',
+//   searchType: 'all',
+//   sort: 'latest',
+//   sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+// }
 
 const initialState = {
   isLoading: true,
@@ -18,14 +18,28 @@ const initialState = {
   page: 1,
   stats: {},
   monthlyApplications: [],
-  ...initialFiltersState,
+  // ...initialFiltersState,
 }
+
 export const getAllJobs = createAsyncThunk(
   'allJobs/getJobs',
   async (_, thunkAPI) => {
     let url = `/jobs`
     try {
       const resp = await customFetch.get(url)
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+export const showStats = createAsyncThunk(
+  'allJobs/showStats',
+  async (_, thunkAPI) => {
+    try {
+      const resp = await customFetch.get('/jobs/stats/')
+      console.log(resp.data)
       return resp.data
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg)
@@ -53,6 +67,18 @@ const allJobsSlice = createSlice({
       state.jobs = payload.jobs
     },
     [getAllJobs.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      toast.error(payload)
+    },
+    [showStats.pending]: (state) => {
+      state.isLoading = true
+    },
+    [showStats.fulfilled]: (state, { payload }) => {
+      state.isLoading = false
+      state.stats = payload.defaultStats
+      state.monthlyApplications = payload.monthlyApplications
+    },
+    [showStats.rejected]: (state, { payload }) => {
       state.isLoading = false
       toast.error(payload)
     },
--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { showStats } from '../../features/allJobs/allJobsSlice'
+
 const Stats = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(showStats())
+    // eslint-disable-next-line
+  }, [])
+
   return <h1>Stats</h1>
 }
 export default Stats
