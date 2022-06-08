345743aa426f1021aed8288436e903e3983f3378
cuongnc <codondemen1102@gmail.com>
2022-06-08 07:52:48 +0700

    demo app functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -78,12 +78,27 @@ const Register = () => {
         {/* toggle button */}
         <button
           type='submit'
-          className='btn btn-block'
+          className='btn btn-block '
           onClick={handleSubmit}
           disabled={isLoading}
         >
           {isLoading ? 'Loading...' : 'submit'}
         </button>
+
+        {/* demo button */}
+        <button
+          type='button'
+          className='btn btn-block btn-hipster'
+          disabled={isLoading}
+          onClick={() => {
+            dispatch(
+              loginUser({ email: 'testUser@test.com', password: 'secret' })
+            )
+          }}
+        >
+          {isLoading ? 'loading...' : 'demo app'}
+        </button>
+
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
           <button type='button' className='member-btn' onClick={toggleMember}>
